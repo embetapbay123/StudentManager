929adfaff75f443186c2bbcf7926ac4434d789c2
embetapbay123 <175529093+embetapbay123@users.noreply.github.com>
2025-12-31 02:10:41 +0700

    Add student model with fields and getter/setter

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import models.Student;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Xin chào Java Backend");
-        System.out.println("Tôi sẽ quyết tâm hoàn thành giai đoạn 1!");
+        Student student = new Student("SV001", "Nguyễn Minh Công", 8.3);
+        // Tạo thử 1 sv
+        System.out.println("Thông tin sinh viên vừa tạo");
+        System.out.println(student); // Không cần thiết phải .toString? chắc là tự convert qua
+        // Thay đổi điểm
+        student.setMark(10);
+        System.out.println("Thông tin sinh viên sau khi đổi thành 10 điểm");
+        System.out.println(student);
     }
 }
---
+++ src/models/Student.java
@@ -0,0 +1,46 @@
+package models;
+
+public class Student {
+    private String id;
+    private String name;
+    private double mark;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getMark() {
+        return mark;
+    }
+
+    public void setMark(double mark) {
+        this.mark = mark;
+    }
+
+    public Student(String id, String name, double mark) {
+        this.id = id;
+        this.name = name;
+        this.mark = mark;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", mark=" + mark +
+                '}';
+    }
+}
