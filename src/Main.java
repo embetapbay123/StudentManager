8c60a536ce83007dca75c190b9ebde55d0ce68bb
embetapbay123 <175529093+embetapbay123@users.noreply.github.com>
2025-12-31 01:46:36 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Xin chào Java Backend");
+        System.out.println("Tôi sẽ quyết tâm hoàn thành giai đoạn 1!");
+    }
+}
