f53eff7e158609fe76745c96e217cd57193f8351
embetapbay123 <175529093+embetapbay123@users.noreply.github.com>
2026-01-01 00:27:01 +0700

    save and read file

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         StudentManager sm = new StudentManager();
         Scanner sc = new Scanner(System.in);
+        sm.loadFromFile();
         int choice;
         do {
             System.out.println("===MENU QUẢN LÝ SINH VIÊN===");
@@ -14,7 +15,8 @@ public class Main {
             System.out.println("2. Xem danh sách sinh viên");
             System.out.println("0. Thoát");
             System.out.print("Lựa chọn của bạn: ");
-            choice = sc.nextInt();
+//            choice = sc.nextInt(); // có 1 lỗi ở đây, buffer đọc luôn dấu \n nhưng thg nextInt k lấy nên bị nhét vào mấy thg sau
+            choice = Integer.parseInt(sc.nextLine());
             switch (choice) {
                 case 1:
                     System.out.println("Nhập thông tin sinh viên");
@@ -31,6 +33,7 @@ public class Main {
                     System.out.println("Lựa chọn không hợp lệ");
             }
         } while (choice != 0);
+        sm.saveToFile();
         sc.close();
     }
 }
--- src/services/StudentManager.java
+++ src/services/StudentManager.java
@@ -2,6 +2,7 @@ package services;
 
 import models.Student;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -27,8 +28,39 @@ public class StudentManager {
         System.out.print("Nhập tên sinh viên: ");
         String name =  scanner.nextLine();
         System.out.print("Nhập điểm sinh viên: ");
-        double mark = scanner.nextDouble();
+        double mark = Double.parseDouble(scanner.nextLine());
 //        scanner.close(); đóng lại là đi luôn bởi vì nó đóng luôn system.in thì những lần đọc sau sẽ k đọc được nữa
         return new Student(id,name,mark);
+    }
+    public void saveToFile() {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("students.txt"))) {
+            for (Student s : students) {
+                bw.write(s.getId() + "," + s.getName() + "," + s.getMark());
+                bw.newLine();
+            }
+            System.out.println("-->Đã lưu dữ liệu sinh viên vào file students.txt");
+        } catch (IOException e) {
+            System.out.println("Lỗi khi lưu file: " + e);
+        }
+    }
+    public void loadFromFile() {
+        File file = new File("students.txt");
+        if (!file.exists()) {
+            System.out.println("-->Không tìm được file students.txt...");
+            return;
+        }
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+                String id = data[0];
+                String name = data[1];
+                double mark = Double.parseDouble(data[2]);
+                this.students.add(new Student(id,name,mark));
+            }
+            System.out.println("-->Đọc dữ liệu và thêm dữ liệu thành công");
+        } catch (IOException e) {
+            System.out.println("Lỗi khi đọc file: " + e);
+        }
     }
 }
